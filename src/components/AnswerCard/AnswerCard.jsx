4efb0707391bfb67ccb9e70d4b28fba9d8bcf29d
HyBeard <Kaptsevich.i@gmail.com>
2020-03-13 06:42:45 +0300

    feat: display hint text in question card if answer was not given

--- src/components/AnswerCard/AnswerCard.jsx
+++ src/components/AnswerCard/AnswerCard.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 
 import AudioPlayer from '../AudioPlayer';
 
-const AnswerCard = ({
-  mixinClass,
-  data: { name, species, description, image, audio },
-}) => {
+const AnswerCard = ({ mixinClass, data }) => {
+  if (data) {
+    const { name, species, description, image, audio } = data;
+
+    return (
+      <div className={`answer-card ${mixinClass}`}>
+        <img src={image} alt="" className="answer-card__img" />
+        <div className="answer-card__main">
+          <h3 className="answer-card__name">{name}</h3>
+          <h4 className="answer-card__species">{species}</h4>
+          <AudioPlayer src={audio} />
+        </div>
+        <p className="answer-card__description">{description}</p>
+      </div>
+    );
+  }
+
   return (
     <div className={`answer-card ${mixinClass}`}>
-      <img src="" alt="" className="answer-card__img" />
-      <div className="answer-card__main">
-        <h3 className="answer-card__name">{name}</h3>
-        <h4 className="answer-card__species">{species}</h4>
-        <AudioPlayer src={audio} />
-      </div>
-      <p className="answer-card__description">{description}</p>
+      <p className="answer-card__description">
+        Послушайте плеер и выберите птицу из списка.
+      </p>
     </div>
   );
 };
